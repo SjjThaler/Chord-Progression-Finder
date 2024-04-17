dc26400f8b0340a6aff2468b73f375ccac358ff6
Stefan Thaler <120387118+SjjThaler@users.noreply.github.com>
2024-04-17 19:36:57 +0200

    Prototype Roadmap

---
+++ prototype.py
@@ -0,0 +1,11 @@
+# Notes Input (idealerweise später über Django)
+	# Hier irgendwie mehrere Chords? Nicht nur einen oder?
+
+# Chord Calculation
+	# Hier müsste ein Algorithmus hin, welcher Noten in Nummern umwandelt
+	# und in Terzen auftürmt
+
+
+# Chord Progression Calculation
+	# Hier müsste ein Algorithmus hin, welcher Akkorde in Skalen einordnet und darüber hinaus
+	# wohl auch Spezialbeziehungen erkennt (Secondary Dominant, Tritonus Substitution)
