e6de289d851ec56662e8bddd6bc44225acbd66f5
Stefan Thaler <120387118+SjjThaler@users.noreply.github.com>
2024-04-18 10:39:27 +0200

    input

--- prototype.py
+++ prototype.py
@@ -1,5 +1,7 @@
 # Notes Input (idealerweise später über Django)
 	# Hier irgendwie mehrere Chords? Nicht nur einen oder?
+a,b,c = input("Please add chord notes.")
+
 
 # Chord Calculation
 	# Hier müsste ein Algorithmus hin, welcher Noten in Nummern umwandelt
