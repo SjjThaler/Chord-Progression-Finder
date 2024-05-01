b69f55867a38492d00d780ce29d8d8ffb9e04a1e
Stefan Thaler <120387118+SjjThaler@users.noreply.github.com>
2024-05-02 00:00:18 +0200

    ...

--- prototype.py
+++ prototype.py
@@ -2,39 +2,8 @@ from timeit import default_timer as timer
 
 # Notes Input (idealerweise später über Django)
 	# Hier irgendwie mehrere Chords? Nicht nur einen oder?
-notes = input("Please give your notes in format: xyz ")
-notes = notes.upper()
-print(notes)
-
-# Dictionary Lösung
-dstart = timer()
-
-dictlist = []
-notdict = {
-	"C": 1,
-	"C#":2,
-	"Db":2,
-	"D": 3,
-	"D#":4,
-	"Eb":4,
-	"E": 5,
-	"E#":6,
-	"Fb":6,
-	"F": 6,
-	"F#":7,
-	"Gb":7,
-	"G": 8,
-	"G#":9,
-	"Ab":10,
-	"A":11,
-	"A#":12,
-	"B":12,
-}
-
-for x in notes:
-	dictlist.append(notdict[x])
-
-
+n1 = input("Gib N1 ein: ")
+n2 = input("Gib N2 ein: ")
 
 def intervall (n1, n2):
     notdict = {
