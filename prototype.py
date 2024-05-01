a79973202b72f0853000b59e703ec141e15737f6
JohnOnline007 <167236528+JohnOnline007@users.noreply.github.com>
2024-05-01 23:55:07 +0200

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -60,9 +60,29 @@ interval_calc()
 	# wohl auch Spezialbeziehungen erkennt (Secondary Dominant, Tritonus Substitution)
 
 def intervall (n1, n2):
-    notes = ["c", "cis", "d", "dis", "e", "f", "fis", "g", "gis", "a", "ais", "b"]
-    note1 = notes.index(n1)
-    note2 = notes.index(n2)
+    notdict = {
+	"C": 1,
+	"C#":2,
+	"Db":2,
+	"D": 3,
+	"D#":4,
+	"Eb":4,
+	"E": 5,
+	"E#":6,
+	"Fb":6,
+	"F": 6,
+	"F#":7,
+	"Gb":7,
+	"G": 8,
+	"G#":9,
+	"Ab":10,
+	"A":11,
+	"A#":12,
+	"B":12,
+}
+
+    note1 = notdict[n1]
+    note2 = notdict[n2]
 
     if note1 > note2:
         note2+=12
