b05d482b2dd1fbe778733877591521003fb493e9
JohnOnline007 <167236528+JohnOnline007@users.noreply.github.com>
2024-05-03 23:02:09 +0200

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -51,24 +51,30 @@ def intervall (n1, n2):
     return note2-note1
 print(notes)
 chord_intervalls = []
-
-for chord in progression
+progression_intervalls = []
+for chord in progression:
 	for i in range(len(chord)-1):
 		chord_intervalls.append(str(intervall(chord[i], chord[i+1])))
+	progression_intervalls.append(chord_intervalls)
 	print(chord_intervalls)
+	chord_intervalls = []
 
-chord_names = {("4", "3"): f"{notes[0]} Dur",
-			   ("5", "4"): f"{notes[1]}/{notes[0]} Dur",
-			   ("3", "5"): f"{notes[2]}/{notes[0]} Dur",
-			   #Moll
-			   ("3", "4"): f"{notes[0]} Moll",
-			   ("5", "3"): f"{notes[1]}/{notes[0]} Moll",
-			   ("4", "5"): f"{notes[2]}/{notes[0]} Moll",
-			   #Dur sept
-			   ("4", "3", "4"): f"{notes[0]}maj7",
-			   ("1", "4", "3"): f"{notes[1]}/{notes[0]}maj7"
+print(progression_intervalls)
 
-}
 
-for i in progression:
-print(chord_names[tuple(chord_intervalls)])
+
+for i in range(len(progression_intervalls)):
+	chord_names = {("4", "3"): f"{progression[i][0]} Dur",
+					("5", "4"): f"{progression[i][1]}/{progression[i][0]} Dur",
+					("3", "5"): f"{progression[i][2]}/{progression[i][0]} Dur",
+					# Moll
+					("3", "4"): f"{progression[i][0]} Moll",
+					("5", "3"): f"{progression[i][1]}/{progression[i][0]} Moll",
+					("4", "5"): f"{progression[i][2]}/{progression[i][0]} Moll",
+					# Dur sept
+					("4", "3", "4"): f"{progression[i][0]}maj7",
+					("1", "4", "3"): f"{progression[i][1]}/{progression[i][0]}maj7"
+	}
+
+
+	print(chord_names[tuple(progression_intervalls[i])])
