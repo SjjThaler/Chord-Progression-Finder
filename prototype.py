d5b401e1724c60cab6a9e24dfd7fb86c9ad40847
JohnOnline007 <167236528+JohnOnline007@users.noreply.github.com>
2024-05-03 22:35:48 +0200

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -1,12 +1,20 @@
 print("Chord indentifier\nStopp with X")
 notes = []
 note = ""
-while note != "X":
-	note = input("Input notes (C#/Cb):").capitalize()
-	notes.append(note)
-notes.remove("X")
+progression = []
+end="X"
+while end == "X":
+	while note != "X":
+		note = input("Input notes (C#/Cb):").capitalize()
+		if note != "X":
+			notes.append(note)
 
+	progression.append(notes)
+	note = ""
+	notes = []
+	end = input("Continue X")
 
+print(progression)
 
 
 def intervall (n1, n2):
@@ -43,16 +51,24 @@ def intervall (n1, n2):
     return note2-note1
 print(notes)
 chord_intervalls = []
-for i in range(len(notes)-1):
-	chord_intervalls.append(str(intervall(notes[i], notes[i+1])))
-print(chord_intervalls)
+
+for chord in progression
+	for i in range(len(chord)-1):
+		chord_intervalls.append(str(intervall(chord[i], chord[i+1])))
+	print(chord_intervalls)
 
 chord_names = {("4", "3"): f"{notes[0]} Dur",
 			   ("5", "4"): f"{notes[1]}/{notes[0]} Dur",
 			   ("3", "5"): f"{notes[2]}/{notes[0]} Dur",
 			   #Moll
-			   ("3", "5"): f"{notes[2]}/{notes[0]} Dur",
+			   ("3", "4"): f"{notes[0]} Moll",
+			   ("5", "3"): f"{notes[1]}/{notes[0]} Moll",
+			   ("4", "5"): f"{notes[2]}/{notes[0]} Moll",
+			   #Dur sept
+			   ("4", "3", "4"): f"{notes[0]}maj7",
+			   ("1", "4", "3"): f"{notes[1]}/{notes[0]}maj7"
 
 }
 
+for i in progression:
 print(chord_names[tuple(chord_intervalls)])
