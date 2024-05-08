41165ea7c06839793dfccc5bb8038eae3feff333
JohnOnline007 <matejka@pm.me>
2024-05-08 19:22:40 +0200

    xy

--- prototype.py
+++ prototype.py
@@ -9,9 +9,17 @@ end="y"
 # Input loop to allow:
 # 1. input of multiple musical notes
 # 2. input of several groups of notes
-end= input("mit x: Beispiel Kadenz (1, 4, 5, 6, 4, 5, 1) verwenden?\nmit y: Mach eigene angaben\nEingabe: ")
+end= input("mit x: Beispiel Kadenz verwenden?\nmit y: Mach eigene angaben\nEingabe: ")
 if end == "x":
-	progression = [['C', 'E', 'G'], ['F', 'A', 'C'], ['G', 'B', 'D'], ['A', 'C', 'E'], ['F', 'A', 'C'], ['G', 'B', 'D'], ['C', 'E', 'G']]
+	kadenz = input("Wähle eine und bestätige mit der Zahl\n1: (1, 4, 5, 6, 4, 5, 1)\n2: (1, 4, 5, 1)\n3: (2, 4, 5, 1)\n4: (1, 6, 4#, 5, 1)\nEingabe: ")
+	if kadenz == "1":
+		progression = [['C', 'E', 'G'], ['F', 'A', 'C'], ['G', 'B', 'D'], ['A', 'C', 'E'], ['F', 'A', 'C'], ['G', 'B', 'D', 'F'], ['G', 'E', 'C']]
+	if kadenz == "2":
+		progression = [['C', 'E', 'G'], ['F', 'A', 'C'], ['G', 'B', 'D'], ['C', 'E', 'G']]
+	if kadenz == "3":
+		progression = [['D', 'F', 'A'], ['F', 'A', 'C'], ['G', 'B', 'D'], ['C', 'E', 'G']]
+	if kadenz == "4":
+		progression = [['C', 'E', 'G'], ['A', 'C', 'E'], ['D', 'F#', 'A'], ['G', 'B', 'D'], ['C', 'E', 'G']]
 while end == "y":
 	while note != "X":
 		note = input("Input notes (C#/Cb):").capitalize()
